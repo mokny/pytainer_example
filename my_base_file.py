cf050337565e68f54e7a8787d03521a85c6cb6bf
mokny <33606734+mokny@users.noreply.github.com>
2023-03-28 16:16:59 +0200

    Update my_base_file.py

--- my_base_file.py
+++ my_base_file.py
@@ -8,6 +8,32 @@ import pathlib
 
 active = True
 
+# This is an example loop, that will run with or without pyTainer.
+def mainLoop():
+    print(config['greeting'])  # 'greeting' was defined at the pytainer.toml file
+    counter = 0
+    while active:
+        counter += 1
+        print("Output of some loop from a non blocking thread - Loop: " + str(counter) + " - Now waiting for " + str(config['sleeptime']) + " seconds")
+        time.sleep(config['sleeptime'])
+
+# This function gets called when the app was started as NON-Standalone
+def pytainer_init(app):
+    # 'pyTainerThread' references the underlying thread. See the docs how to use.
+    print("I am running as a pyTainer Module.")
+    print("Init function called.")
+    global config
+    config = app.config
+    
+    mainLoop()
+
+# This function gets called when the app was stopped as NON-Standalone
+def pytainer_stop(app):
+    print("Stop function called.")
+    global active
+    active = False
+
+# This detects if the app is running inside pyTainer.
 if not 'pytainerserver' in sys.modules:
     print("Not running as pyTainer Module, initializing manually.")
     sys.path.insert(0, str(pathlib.Path(__file__).parent.resolve()) + '/../../ipc')
@@ -21,26 +47,5 @@ if not 'pytainerserver' in sys.modules:
 
 
 
-def mainLoop():
-    print(config['greeting'])  # 'greeting' was defined at the pytainer.toml file
-    counter = 0
-    while active:
-        counter += 1
-        print("Output of some loop from a non blocking thread - Loop: " + str(counter) + " - Now waiting for " + str(config['sleeptime']) + " seconds")
-        time.sleep(config['sleeptime'])
-
-def pytainer_init(app):
-    # 'pyTainerThread' references the underlying thread. See the docs how to use.
-    print("I am running as a pyTainer Module.")
-    print("Init function called.")
-    global config
-    config = app.config
-    
-    mainLoop()
-
-def pytainer_stop(app):
-    print("Stop function called.")
-    global active
-    active = False
 
 
